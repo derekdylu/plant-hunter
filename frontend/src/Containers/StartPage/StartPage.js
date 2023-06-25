af5eeda64b9a9f09aa1d6ee403d4ee0c7bd4e949
Derek Lu <derekdylu@gmail.com>
2023-06-25 19:43:17 +0800

    feat: try adding responsive startpage

--- frontend/src/Containers/StartPage/StartPage.js
+++ frontend/src/Containers/StartPage/StartPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import classnames from 'classnames'
-// import styles from './styles.module.scss'
+import styles from './styles.module.scss'
 
 import book from '../../Assets/StartPage/book.png'
 import cb_mr from '../../Assets/StartPage/cardboard - middle right.png'
@@ -17,18 +17,27 @@ import shovel from '../../Assets/StartPage/shovel - upper.png'
 import 台灣喜普鞋蘭 from '../../Assets/StartPage/台灣喜普鞋蘭.png'
 import 台灣野牡丹藤 from '../../Assets/StartPage/台灣野牡丹藤.png'
 import 桃紅蝴蝶蘭  from '../../Assets/StartPage/桃紅蝴蝶蘭.png'
+import background from '../../Assets/StartPage/background.png'
 
 const StartPage = () => {
   return (
-    <div className='container'>
-      <div className='flex flex-row items-center justify-center h-screen'>
-        <div>
-          aosdif
-        </div>
-        <div>
-          asodifjasdf
-        </div>
-      </div>
+    <div className='container w-screen h-screen'>
+      <img src={shovel} className={classnames(styles.shovel)} alt='shovel' />
+      <img src={pr_ur} className={classnames(styles.pr_ur)} alt='pr_ur' />
+      <img src={台灣野牡丹藤} className={classnames(styles.台灣野牡丹藤)} alt='台灣野牡丹藤' />
+      <img src={cb_ul} className={classnames(styles.cb_ul)} alt='cb_ul' />
+      <img src={桃紅蝴蝶蘭} className={classnames(styles.桃紅蝴蝶蘭)} alt='桃紅蝴蝶蘭' />
+      <img src={pr_br} className={classnames(styles.pr_br)} alt='pr_br' />
+      <img src={mt_bl} className={classnames(styles.mt_bl)} alt='mt_bl' />
+      <img src={book} className={classnames(styles.book)} alt='book' />
+      <img src={np} className={classnames(styles.np)} alt='np' />
+      <img src={台灣喜普鞋蘭} className={classnames(styles.台灣喜普鞋蘭)} alt='台灣喜普鞋蘭' />
+      <img src={pr_bl} className={classnames(styles.pr_bl)} alt='pr_bl' />
+      <img src={pr_m} className={classnames(styles.pr_m)} alt='pr_m' />
+      <img src={cb_mr} className={classnames(styles.cb_mr)} alt='cb_mr' />
+      <img src={mt_mr} className={classnames(styles.mt_mr)} alt='mt_mr' />
+      <img src={logotype} className={classnames(styles.logotype)} alt='logotype'/>
+      <img src={background} className={classnames(styles.background)} alt='background' />
     </div>
   )
 }
