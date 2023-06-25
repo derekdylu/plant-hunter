20315b96796154a0d46fed0e26bdaa4962745fd8
Derek Lu <derekdylu@gmail.com>
2023-06-25 17:47:29 +0800

    init

---
+++ frontend/src/Containers/StartPage/StartPage.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import classnames from 'classnames'
+// import styles from './styles.module.scss'
+
+import book from '../../Assets/StartPage/book.png'
+import cb_mr from '../../Assets/StartPage/cardboard - middle right.png'
+import cb_ul from '../../Assets/StartPage/cardboard - upper left.png'
+import logotype from '../../Assets/StartPage/logotype.png'
+import mt_bl from '../../Assets/StartPage/mountain - bottom left.png'
+import mt_mr from '../../Assets/StartPage/mountain - middle right - dark ver.png'
+import np from '../../Assets/StartPage/note paper - bottom.png'
+import pr_bl from '../../Assets/StartPage/Paper Rip - bottom left.png'
+import pr_br from '../../Assets/StartPage/Paper Rip - bottom right.png'
+import pr_m from '../../Assets/StartPage/Paper Rip - middle.png'
+import pr_ur from '../../Assets/StartPage/Paper Rip - upper right.png'
+import shovel from '../../Assets/StartPage/shovel - upper.png'
+import 台灣喜普鞋蘭 from '../../Assets/StartPage/台灣喜普鞋蘭.png'
+import 台灣野牡丹藤 from '../../Assets/StartPage/台灣野牡丹藤.png'
+import 桃紅蝴蝶蘭  from '../../Assets/StartPage/桃紅蝴蝶蘭.png'
+
+const StartPage = () => {
+  return (
+    <div className='container'>
+      <div className='flex flex-row items-center justify-center h-screen'>
+        <div>
+          aosdif
+        </div>
+        <div>
+          asodifjasdf
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default StartPage
